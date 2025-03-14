463721703e241de9a9916294701e0143b9856853
IngaGd <inga.gudaite@gmail.com>
2025-03-14 11:14:15 +0700

    Add city celect

--- client/src/common/components/CitiesDropDown/CitiesDropDown.jsx
+++ client/src/common/components/CitiesDropDown/CitiesDropDown.jsx
@@ -11,12 +11,30 @@ export function CitiesDropDown() {
     city.cityName.slice(1).startsWith(citySearch.slice(1))
   );
 
+  const [selectedCity, setSelectedCity] = useState([]);
+
+  const handleClick = (city) => {
+    console.log("selected id: ", city);
+    setSelectedCity({ id: city.id, code: city.cityCode, name: city.cityName });
+  };
+
   return (
     <div>
-      <Input citySearch={citySearch} setCitySearch={setCitySearch} />
+      <Input
+        citySearch={citySearch}
+        setCitySearch={setCitySearch}
+        selectedCity={selectedCity}
+      />
+      cityName: {selectedCity.name}
       <div>
         {filteredCities?.map((city) => (
-          <div key={city.id}>{city.cityName}</div>
+          <div
+            key={city.id}
+            value={selectedCity}
+            onClick={() => handleClick(city)}
+          >
+            {city.cityName}
+          </div>
         ))}
       </div>
     </div>
