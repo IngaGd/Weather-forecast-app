b1e9f3589a1009b4c0d38360afc8eb8df5f0007d
IngaGd <inga.gudaite@gmail.com>
2025-03-15 19:17:32 +0700

    Add server data logs to json file

--- server/controllers/logsController.js
+++ server/controllers/logsController.js
@@ -1,8 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
 exports.logUserAction = async (req, res) => {
   const { cityCode, cityName } = req.body;
   const timeStamp = new Date().toString();
+  const logData = { timeStamp, cityCode, cityName };
+  const filePath = path.join(__dirname, "../data/logs.json");
 
   console.log(`User selected city: ${cityCode}, ${cityName}, at ${timeStamp}`);
 
-  res.status(200).json({ message: "ok" });
+  try {
+    let logs = [];
+
+    let fileData = fs.readFileSync(filePath, "utf8");
+    fileData = JSON.parse(fileData);
+
+    if (logData) {
+      logs.push(logData);
+    }
+
+    fs.writeFileSync(filePath, JSON.stringify(logs), "utf8");
+    res.status(200).json({ message: "Data saved to json db" });
+  } catch (error) {
+    console.error("Error writing to logs file:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
