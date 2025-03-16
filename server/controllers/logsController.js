9bb0d68eb2441908a2cec93da6cbd246381a77c7
IngaGd <inga.gudaite@gmail.com>
2025-03-16 18:44:50 +0700

    Fix log data functionality - not override previous data

--- server/controllers/logsController.js
+++ server/controllers/logsController.js
@@ -2,22 +2,23 @@ const fs = require("fs");
 const path = require("path");
 
 exports.logUserAction = async (req, res) => {
-  const { cityCode, cityName } = req.body;
+  const { cityCode } = req.body;
+  console.log("code:", cityCode);
+
   const timeStamp = new Date().toString();
-  const logData = { timeStamp, cityCode, cityName };
+
+  const logData = { timeStamp, cityCode };
   const filePath = path.join(__dirname, "../data/logs.json");
 
-  console.log(`User selected city: ${cityCode}, ${cityName}, at ${timeStamp}`);
+  console.log(`User selected city: ${cityCode} at ${timeStamp}`);
 
   try {
     let logs = [];
 
-    let fileData = fs.readFileSync(filePath, "utf8");
-    fileData = JSON.parse(fileData);
+    const fileData = fs.readFileSync(filePath, "utf8");
+    logs = JSON.parse(fileData);
 
-    if (logData) {
-      logs.push(logData);
-    }
+    logs.push(logData);
 
     fs.writeFileSync(filePath, JSON.stringify(logs), "utf8");
     res.status(200).json({ message: "Data saved to json db" });
